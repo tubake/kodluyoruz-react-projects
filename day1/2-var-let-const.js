06017c4c8e39e37f4bdf214d4498943fb2d6c3b5
tubake <tuba.bezek@bil.omu.edu.tr>
2021-02-18 15:45:41 +0300

    variables names

---
+++ day1/2-var-let-const.js
@@ -0,0 +1,30 @@
+//1. declaration
+// var surname = 'bezek';
+// var surname = 'ekinci';
+
+// let name = 'tuba';
+// // let name = 'pınar';
+
+// console.log(surname);
+// console.log(name);
+
+// 2. scope
+// for (var j = 0; j < 10; j++) {}
+
+// console.log(j);
+
+// for (let i = 0; i < 10; i++) {}
+
+// console.log(i);
+
+// 3.
+// const pi = 3;
+// pi = 3.14;
+// console.log(pi);
+
+// const users = ['Sude', 'Beril', 'Melis'];
+// users.push('Mirza');
+
+users = ['Mirza'];
+
+console.log(users);
