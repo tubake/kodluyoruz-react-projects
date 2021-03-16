b728188fd9b3d7b429797c6bf56168fed109ad04
tubake <tuba.bezek@bil.omu.edu.tr>
2021-03-17 00:25:57 +0300

    commonjs modules

---
+++ day2/2-modules/index.js
@@ -0,0 +1,10 @@
+const {topla ,cikar ,arr} = require("./modules/myModule");
+
+const { lower, sluger } = require("./modules/strConvert");
+
+// console.log(cikar(9,4));
+// console.log(topla(2,4));
+// console.log(arr);
+
+console.log(lower("React Native"));
+console.log(sluger("React Native Test"));
