ba61cc42e2df87f1644ccd49e6d7c7584e7c55b2
tubake <tuba.bezek@bil.omu.edu.tr>
2021-03-24 23:50:19 +0300

    react useState()

---
+++ day3/3-state-example/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+function App() {
+	const [isVisible, setIsVisible] = useState(false);
+	const [users, setUsers] = useState([
+		{ name: 'Metin', age: 22 },
+		{ name: 'Hande', age: 24 },
+	]);
+
+	const handleClick = () => {
+		setUsers((prev) => [...prev, { name: 'Seda', age: 33 }]);
+
+		//setUsers([...users, { name: 'Burak', age: 33 }]);
+	};
+
+	const handleToggle = () => setIsVisible(!isVisible);
+
+	return (
+		<div>
+			<button onClick={handleToggle}>Toggle {isVisible ? 'off' : 'on'}</button>
+
+			{isVisible && (
+				<>
+					{users.map((user, i) => (
+						<li key={i}>{user.name}</li>
+					))}
+					<hr />
+					<button onClick={handleClick}>Add user</button>
+				</>
+			)}
+		</div>
+	);
+}
+
+export default App;
