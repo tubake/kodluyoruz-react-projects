b921842f220561c98e37aad24faa057429a92e17
tubake <tuba.bezek@bil.omu.edu.tr>
2021-03-25 22:25:20 +0300

    native props

--- day3/3-state-example/src/components/UserList.js
+++ day3/3-state-example/src/components/UserList.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 
-function UserList({ users }) {
-	console.log({ users });
+import Button from './Button';
+
+function UserList({ users, setUsers }) {
+	const [name, setName] = useState('');
+
+	const handleClick = () => {
+		alert('a');
+		if (name) {
+			setUsers((prev) => [...prev, { name, age: 33 }]);
+			setName('');
+		}
+	};
+
+	const handleChangeName = (event) => setName(event.target.value);
+
 	return (
-		<ul>
-			{users.map((user, i) => (
-				<li key={i}>{user} </li>
-			))}
-		</ul>
+		<>
+			<ul>
+				{users.map((user, i) => (
+					<li key={i}>{user} </li>
+				))}
+			</ul>
+			<input value={name} onChange={handleChangeName} />
+			<Button onClick={handleClick} text="Click" style={{ color: 'red' }} />
+		</>
 	);
 }
 
