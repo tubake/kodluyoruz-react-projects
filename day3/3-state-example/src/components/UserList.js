fb6ff5e27a8c3ed58238f3eca29e1ed5fa5700fd
tubake <tuba.bezek@bil.omu.edu.tr>
2021-03-27 16:59:36 +0300

    useEffect for users array

--- day3/3-state-example/src/components/UserList.js
+++ day3/3-state-example/src/components/UserList.js
@@ -1,27 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Button from './Button';
 
 function UserList({ users, setUsers }) {
 	const [name, setName] = useState('');
 
+	useEffect(() => {
+		setName('');
+	}, [users]);
+
 	const handleClick = () => {
-		alert('a');
+		//alert('a');
 		if (name) {
 			setUsers((prev) => [...prev, { name, age: 33 }]);
-			setName('');
+			//setName('');
 		}
 	};
 
 	const handleChangeName = (event) => setName(event.target.value);
 
+	const handleRemove = (userId) => {
+		const filtered = users.filter((user) => user.id !== userId);
+		setUsers(filtered);
+	};
+
 	return (
 		<>
-			<ul>
-				{users.map((user, i) => (
-					<li key={i}>{user} </li>
-				))}
-			</ul>
+			{users.length > 0 ? (
+				<ul>
+					{users.map((user, i) => (
+						<li key={i}>
+							{i + 1} - {user.name}{' '}
+							<span
+								onClick={() => handleRemove(user.id)}
+								style={{ color: 'blue', cursor: 'pointer' }}
+							>
+								(x)
+							</span>
+						</li>
+					))}
+				</ul>
+			) : (
+				<div> Henüz kullanıcı bulunmuyor.</div>
+			)}
 			<input value={name} onChange={handleChangeName} />
 			<Button onClick={handleClick} text="Click" style={{ color: 'red' }} />
 		</>
